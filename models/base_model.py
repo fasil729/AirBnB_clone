12a29b7dd2f0fbb95b20ee424cd593fb78907177
fasika <fasikafikadu38@gmail.com>
2022-11-25 13:53:57 -0800

    update basemodel by args and **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,20 @@ from datetime import datetime
 class BaseModel:
     """defines all common attributes/methods for other classes"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    forma = "%Y-%m-%dT%H:%M:%S.%f"
+
+    def __init__(self, *args, **kwargs):
+        """intializer"""
+        if kwargs:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], __class__.forma)
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], __class__.forma)
+            for key in kwargs.keys():
+                if key != "__class__":
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"

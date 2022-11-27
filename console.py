eca977274d5c743358b40a06b5c20e2d088f834f
fasika <fasikafikadu38@gmail.com>
2022-11-27 14:27:32 -0800

    test files with validating pep8

---
+++ console.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""the console to test my works"""
+import cmd
+from models import BaseModel
+
+class_names = ["BaseModel"]
+
+
+class HBNBCommand(cmd.Cmd):
+    """a class use for console """
+    prompt = " (hbnb) "
+
+    def emptyline(self):
+        """empty line command"""
+        print(end="")
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """Quit command to exit the program"""
+        print()
+        return True
+
+    def do_create(self, obj=None):
+        if not obj:
+            print("** class name missing **")
+        else:
+            if obj in class_names:
+                new = eval(obj)()
+                new.save()
+                print(new.id)
+            else:
+                print("** class doesn't exist **")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
